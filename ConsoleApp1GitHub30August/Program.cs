1acbd4013b31d9c53567fdf1508522d7ec08fd33
Mikkel Hansen <mikk075q@mail.hansenberg.dk>
2024-08-30 09:08:47 +0200

    Add project files.

---
+++ ConsoleApp1GitHub30August/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp1GitHub30August
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
